3c3121d1a9f2d64d46314579920ef6ea1b859e26
Larry Myers <larry@larrymyers.com>
2025-11-04 09:14:35 -0600

    Remove fetch mock.
    No longer needed since wordList is injected into getPossibleWords.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,6 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vitest/config";
-import { readFileSync } from "fs";
-import { join } from "path";
 
 export default defineConfig({
-  test: {
-    /* for example, use global to avoid globals imports (describe, test, expect): */
-    // globals: true,
-    setupFiles: ['./vitest.setup.ts'],
-  },
+  test: {},
 });
